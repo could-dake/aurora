8f7f24bd9fbc08eec3e62c844262cc3d6cb38614
大可 <2840176273@qq.com>
2026-05-19 14:36:39 +0800

    fix: separate postcss config for Vite

--- aurora-vue/aurora-blog/vite.config.js
+++ aurora-vue/aurora-blog/vite.config.js
@@ -1,19 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
-    }
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer]
     }
   },
   build: {
