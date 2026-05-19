f0d3a4edf0da666522931eceffcbbcd193481cf4
大可 <2840176273@qq.com>
2026-05-19 14:26:03 +0800

    fix: use ESM imports in vite.config.js

--- aurora-vue/aurora-blog/vite.config.js
+++ aurora-vue/aurora-blog/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
   plugins: [vue()],
@@ -11,10 +13,7 @@ export default defineConfig({
   },
   css: {
     postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer')
-      ]
+      plugins: [tailwindcss, autoprefixer]
     }
   },
   build: {
